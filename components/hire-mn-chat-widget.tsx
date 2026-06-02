14b161216d79e8a3506bc485046adaccfafd68a9
v0agent <it+v0agent@vercel.com>
2026-06-02 04:06:45 +0000

    feat: merge AnalysisResults and AnalysisCard in hire-mn-chat-widget

--- components/hire-mn-chat-widget.tsx
+++ components/hire-mn-chat-widget.tsx
@@ -4,8 +4,8 @@ import { useState, useRef, useEffect } from "react"
 import { MessageFeedback } from './message-feedback'
 import { ConversationSidebar } from './conversation-sidebar'
 import { SplineMascot } from './spline-mascot'
-import { AnalysisResults } from './analysis-results'
 import { AnalysisResults, AnalysisCard } from './analysis-results'
+
 import {
   Conversation,
   createNewConversation,
